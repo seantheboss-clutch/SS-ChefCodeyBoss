8b6c741825e601c94ce861880bd5852aeeda5b09
CodeNinjasLehi <codeninja2198@outlook.com>
2024-03-08 16:32:23 -0700

    Move it! Move it!
    Player is moving! This is good! I just need to finalize the movement of the hands, the GUI, the directional light, and the scenes, and I'll be almost done.

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
+    public int speed = 1;
+    public int handCurrent = 1;
+    public Vector3 rrotation;
+    public Quaternion rotation;
+    public int rrot = 270;
+    public int lrot = -90;
+    public Rigidbody playerrb;
+    public Rigidbody rhandrb;
+    public Rigidbody lhandrb;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        handCurrent = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Movement
+        if(Input.GetKeyDown("up"))
+        {
+            playerrb.velocity = transform.TransformDirection(Vector3.right * speed);
+        } else if(Input.GetKeyDown("down"))
+        {
+            playerrb.velocity = transform.TransformDirection(Vector3.left * speed);
+        }
+        //Rotation
+        if(Input.GetKey("right"))
+        {
+            rotation = Quaternion.Euler(0, rrot, 0);
+            rrotation = new Vector3(0, rotation.y, 0);
+            player.transform.Rotate(rrotation);
+        }else if(Input.GetKey("left"))
+        {
+            rotation = Quaternion.Euler(0, lrot, 0);
+            rrotation = new Vector3(0, rotation.y, 0);
+            player.transform.Rotate(rrotation);
+        }
+        //Zeros velocity
+        if(Input.GetKey("0"))
+        {
+            playerrb.velocity = new Vector3(0,0,0);
+        }
+        //moves hands
+        if (Input.GetKeyDown("8"))
+        {
+            if(handCurrent == 0)
+            {
+                lhandrb.velocity = transform.TransformDirection(Vector3.right * speed);
+            } else
+            {
+                rhandrb.velocity = transform.TransformDirection(Vector3.right * speed);
+            }
+        } 
+        if (Input.GetKeyDown("2"))
+        {
+            playerrb.velocity = transform.TransformDirection(Vector3.down * speed);
+        }
+        if(Input.GetKeyDown("l"))
+        {
+            handCurrent = 0;
+        }
     }
 }
